e9653abee3184c151cd07c99312226f3fe3bd9ef
Maham Ijaz <FA19-BSE-039@cuilahore.edu.pk>
2022-03-26 08:32:16 -0700

    Code

---
+++ event.js
@@ -0,0 +1,19 @@
+function btnClicked() 
+{
+    console.log("button CLicked");
+    var btn = document.querySelector("button");
+    btn.onclick = ()=>{
+            window.print();
+        }
+}
+function doBindings() {
+            var btn = document.getElementById("myBtn");
+            console.log(btn);
+            btn.onclick = btnClicked;
+          }
+
+window.onload = doBindings;
+console.log("Time Out");
+
+
+
